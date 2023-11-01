5206a3d15e2ed2dc4911f07e3f81cdeb229e7723
gopps95 <shankargopi7@gmail.com>
2023-11-01 19:02:10 +0530

    changed

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// Declare an array to store previous results
+var resultsArray = [];
+
 function calculatePriority() {
     var o = parseFloat(document.getElementById("o").value);
     var s = parseFloat(document.getElementById("s").value);
     var d = parseFloat(document.getElementById("d").value);
 
     var result = o * s * d;
-    var priority = "Low";
+    resultsArray.push(result);
 
-    if (result > 50) {
-        priority = "High";
-    } else if (result > 20) {
-        priority = "Medium";
-    }
+    // Sort the results array in descending order
+    resultsArray.sort(function (a, b) {
+        return b - a;
+    });
+
+    // Find the priority of the current result
+    var priority = resultsArray.indexOf(result) + 1;
 
     document.getElementById("result").textContent = result;
-    document.getElementById("priority").textContent = priority;
+    document.getElementById("priority").textContent = "Priority: " + priority;
 }
